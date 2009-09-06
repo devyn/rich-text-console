211aed956fa3853f3654188c98fa7799662d8449
Devyn Cairns <devyn.cairns@gmail.com>
2009-09-06 15:01:37 -0700

    like, functions???

--- experiments/syntax.rb
+++ experiments/syntax.rb
@@ -12,11 +12,11 @@ require 'rtc/rtdl'
 Shoes.app do
   background black
 
-  @string = "\e[b,fc=22ccff]Shoes\e[/].app \e[fc=dd9900]do\e[/]
+  @string = "\e[.cl 002222]\e[b,fc=22ccff]Shoes\e[/].app \e[fc=dd9900]do\e[/]
   \e[fc=999999,i]# this is pure Shoes awesomeness.\e[/]
   \e[fc=dd3300]button\e[/](\e[fc=66dd66]\e[fc=dd9900]\"\e[/]Click Me!\e[fc=dd9900]\"\e[//]) \e[bc=333333]{\e[/]
     \e[fc=dd3300]alert\e[/] \e[fc=66dd66]\e[fc=dd9900]\"\e[/]You clicked me!\e[fc=dd9900]\"\e[//]
-  \e[bc=333333]}\e[/]\n\e[fc=dd9900]end\e[/]\e[b]"
+  \e[bc=333333]}\e[/]\n\e[fc=dd9900]end\e[/]"
 
   @parser = RichTextConsole::RTDLParser.new
 
